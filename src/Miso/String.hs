9f3683833fdb8f55628e0fe5a3ccba25c267b95e
Clinton Mead <clinton.mead@grc-solutions.com>
2026-08-11 16:42:32 +1000

    Make `ToMisoString`/`FromMisoString` instances for every type that has a defined `ToHttpApiData`/`FromHttpApiData` instances
    Only special cases are `ToMisoString` `Int32`/`Int16`/`Int8`/`Word32`/`Word16`/`Word8`  instances which can use the FFI call `toString_Int`/`toString_Word` which is presumably faster when compiling to Javascript/WebAssembly.
    
    Note that `Int64`/`Word64` can not use this call because GHC currently compiles to 32 bit WebAssembly (hence 32 bit ints) and numbers in Javascript are represented as doubles, which only have 53 bits of integer precision (resulting in larger integers not overflowing but being rounded to powers of 2).
    
    Existing instances left unaffected. This may result in some inconsistencies at least in error messages from `FromMisoString` instances. We should probably consolidate existing instances to use `ToHttpApiData`/`FromHttpApiData` when there's no obvious optimisation if we want to go this way.

--- src/Miso/String.hs
+++ src/Miso/String.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE CPP               #-}
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DerivingVia       #-}
+{-# LANGUAGE StandaloneDeriving #-}
 -----------------------------------------------------------------------------
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 -----------------------------------------------------------------------------
@@ -40,9 +42,15 @@ module Miso.String
   ) where
 ----------------------------------------------------------------------------
 import           Control.Exception
+import           Control.Applicative (Const)
+import           Data.Bifunctor (bimap)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Builder as B
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.Fixed as F
+import           Data.Functor.Identity (Identity)
+import           Data.Int (Int8, Int16, Int32, Int64)
+import           Data.Monoid (All, Any, Dual, First, Last, Product, Sum)
 #ifdef VANILLA
 import           Data.Text hiding (show, elem)
 #else
@@ -55,9 +63,27 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Encoding as LT
+import qualified Data.Semigroup as Semi
+import           Data.Tagged (Tagged)
+import           Data.Time.Calendar.Month.Compat (Month)
+import           Data.Time.Calendar.Quarter.Compat (Quarter, QuarterOfYear)
+import           Data.Time.Compat (Day, DayOfWeek, LocalTime, NominalDiffTime,
+                                   TimeOfDay, UTCTime, ZonedTime)
+import qualified Data.UUID.Types as UUID
+import           Data.Version (Version)
+import           Data.Void (Void)
+import           Data.Word (Word8, Word16, Word32, Word64)
+import           Numeric.Natural (Natural)
 ----------------------------------------------------------------------------
 import           Miso.DSL.FFI
 ----------------------------------------------------------------------------
+import           Web.HttpApiData
+                   ( LenientData
+                   , ToHttpApiData (toUrlPiece)
+                   , FromHttpApiData (parseUrlPiece)
+                   )
+import           Web.Cookie (SetCookie)
+-----------------------------------------------------------------------------
 -- | The primary string type in Miso applications.
 --
 -- * @VANILLA@ (server\/SSR build): alias for 'Data.Text.Text'
@@ -147,9 +173,21 @@ instance ToMisoString Double where
 ----------------------------------------------------------------------------
 instance ToMisoString Int where
   toMisoString = toString_Int
+instance ToMisoString Int8 where
+  toMisoString = toString_Int . fromIntegral
+instance ToMisoString Int16 where
+  toMisoString = toString_Int . fromIntegral
+instance ToMisoString Int32 where
+  toMisoString = toString_Int . fromIntegral
 ----------------------------------------------------------------------------
 instance ToMisoString Word where
   toMisoString = toString_Word
+instance ToMisoString Word8 where
+  toMisoString = toString_Word . fromIntegral
+instance ToMisoString Word16 where
+  toMisoString = toString_Word . fromIntegral
+instance ToMisoString Word32 where
+  toMisoString = toString_Word . fromIntegral
 ----------------------------------------------------------------------------
 #ifndef VANILLA
 instance FromMisoString MisoString where
@@ -206,3 +244,133 @@ instance FromMisoString Float where
       Nothing -> Left ("fromMisoString Float: could not parse " <> unpack string)
       Just x -> Right x
 ----------------------------------------------------------------------------
+newtype ViaHttpApiData a = ViaHttpApiData { unMisoViaHttpApiData :: a }
+----------------------------------------------------------------------------
+instance ToHttpApiData a => ToMisoString (ViaHttpApiData a) where
+  toMisoString = ms . toUrlPiece . unMisoViaHttpApiData
+----------------------------------------------------------------------------
+instance FromHttpApiData a => FromMisoString (ViaHttpApiData a) where
+  fromMisoStringEither = bimap fromMisoString ViaHttpApiData . parseUrlPiece . fromMisoString
+----------------------------------------------------------------------------
+-- ToHttpApiData instances
+deriving via (ViaHttpApiData ()) instance ToMisoString ()
+deriving via (ViaHttpApiData Version) instance ToMisoString Version
+deriving via (ViaHttpApiData Void) instance ToMisoString Void
+deriving via (ViaHttpApiData Natural) instance ToMisoString Natural
+deriving via (ViaHttpApiData Bool) instance ToMisoString Bool
+deriving via (ViaHttpApiData Ordering) instance ToMisoString Ordering
+-- The Int64, Word64 and Integer instances will overflow 32 bit WASM
+-- and lose precision inJavascript ints (which are represented as doubles, 
+-- but only have 53 bits of integer precision). 
+-- So we can't use the primitive `toString_Word`. 
+deriving via (ViaHttpApiData Int64) instance ToMisoString Int64
+deriving via (ViaHttpApiData Integer) instance ToMisoString Integer
+deriving via (ViaHttpApiData Word64) instance ToMisoString Word64
+deriving via (ViaHttpApiData (F.Fixed a))
+  instance F.HasResolution a => ToMisoString (F.Fixed a)
+deriving via (ViaHttpApiData Day) instance ToMisoString Day
+deriving via (ViaHttpApiData TimeOfDay) instance ToMisoString TimeOfDay
+deriving via (ViaHttpApiData LocalTime) instance ToMisoString LocalTime
+deriving via (ViaHttpApiData ZonedTime) instance ToMisoString ZonedTime
+deriving via (ViaHttpApiData UTCTime) instance ToMisoString UTCTime
+deriving via (ViaHttpApiData DayOfWeek) instance ToMisoString DayOfWeek
+deriving via (ViaHttpApiData QuarterOfYear) instance ToMisoString QuarterOfYear
+deriving via (ViaHttpApiData Quarter) instance ToMisoString Quarter
+deriving via (ViaHttpApiData Month) instance ToMisoString Month
+deriving via (ViaHttpApiData NominalDiffTime) instance ToMisoString NominalDiffTime
+deriving via (ViaHttpApiData All) instance ToMisoString All
+deriving via (ViaHttpApiData Any) instance ToMisoString Any
+deriving via (ViaHttpApiData (Dual a))
+  instance ToHttpApiData a => ToMisoString (Dual a)
+deriving via (ViaHttpApiData (Sum a))
+  instance ToHttpApiData a => ToMisoString (Sum a)
+deriving via (ViaHttpApiData (Product a))
+  instance ToHttpApiData a => ToMisoString (Product a)
+deriving via (ViaHttpApiData (First a))
+  instance ToHttpApiData a => ToMisoString (First a)
+deriving via (ViaHttpApiData (Last a))
+  instance ToHttpApiData a => ToMisoString (Last a)
+deriving via (ViaHttpApiData (Semi.Min a))
+  instance ToHttpApiData a => ToMisoString (Semi.Min a)
+deriving via (ViaHttpApiData (Semi.Max a))
+  instance ToHttpApiData a => ToMisoString (Semi.Max a)
+deriving via (ViaHttpApiData (Semi.First a))
+  instance ToHttpApiData a => ToMisoString (Semi.First a)
+deriving via (ViaHttpApiData (Semi.Last a))
+  instance ToHttpApiData a => ToMisoString (Semi.Last a)
+deriving via (ViaHttpApiData (Either a b))
+  instance (ToHttpApiData a, ToHttpApiData b) => ToMisoString (Either a b)
+deriving via (ViaHttpApiData SetCookie) instance ToMisoString SetCookie
+deriving via (ViaHttpApiData (Tagged b a))
+  instance ToHttpApiData a => ToMisoString (Tagged b a)
+deriving via (ViaHttpApiData (Const a b))
+  instance ToHttpApiData a => ToMisoString (Const a b)
+deriving via (ViaHttpApiData (Identity a))
+  instance ToHttpApiData a => ToMisoString (Identity a)
+deriving via (ViaHttpApiData UUID.UUID) instance ToMisoString UUID.UUID
+----------------------------------------------------------------------------
+-- FromHttpApiData instances
+deriving via (ViaHttpApiData ()) instance FromMisoString ()
+deriving via (ViaHttpApiData Char) instance FromMisoString Char
+deriving via (ViaHttpApiData Version) instance FromMisoString Version
+deriving via (ViaHttpApiData Void) instance FromMisoString Void
+deriving via (ViaHttpApiData Natural) instance FromMisoString Natural
+deriving via (ViaHttpApiData Bool) instance FromMisoString Bool
+deriving via (ViaHttpApiData Ordering) instance FromMisoString Ordering
+deriving via (ViaHttpApiData Int8) instance FromMisoString Int8
+deriving via (ViaHttpApiData Int16) instance FromMisoString Int16
+deriving via (ViaHttpApiData Int32) instance FromMisoString Int32
+deriving via (ViaHttpApiData Int64) instance FromMisoString Int64
+deriving via (ViaHttpApiData Integer) instance FromMisoString Integer
+deriving via (ViaHttpApiData Word8) instance FromMisoString Word8
+deriving via (ViaHttpApiData Word16) instance FromMisoString Word16
+deriving via (ViaHttpApiData Word32) instance FromMisoString Word32
+deriving via (ViaHttpApiData Word64) instance FromMisoString Word64
+deriving via (ViaHttpApiData (F.Fixed a))
+  instance F.HasResolution a => FromMisoString (F.Fixed a)
+deriving via (ViaHttpApiData Day) instance FromMisoString Day
+deriving via (ViaHttpApiData TimeOfDay) instance FromMisoString TimeOfDay
+deriving via (ViaHttpApiData LocalTime) instance FromMisoString LocalTime
+deriving via (ViaHttpApiData ZonedTime) instance FromMisoString ZonedTime
+deriving via (ViaHttpApiData UTCTime) instance FromMisoString UTCTime
+deriving via (ViaHttpApiData DayOfWeek) instance FromMisoString DayOfWeek
+deriving via (ViaHttpApiData NominalDiffTime) instance FromMisoString NominalDiffTime
+deriving via (ViaHttpApiData Month) instance FromMisoString Month
+deriving via (ViaHttpApiData Quarter) instance FromMisoString Quarter
+deriving via (ViaHttpApiData QuarterOfYear) instance FromMisoString QuarterOfYear
+deriving via (ViaHttpApiData All) instance FromMisoString All
+deriving via (ViaHttpApiData Any) instance FromMisoString Any
+deriving via (ViaHttpApiData (Dual a))
+  instance FromHttpApiData a => FromMisoString (Dual a)
+deriving via (ViaHttpApiData (Sum a))
+  instance FromHttpApiData a => FromMisoString (Sum a)
+deriving via (ViaHttpApiData (Product a))
+  instance FromHttpApiData a => FromMisoString (Product a)
+deriving via (ViaHttpApiData (First a))
+  instance FromHttpApiData a => FromMisoString (First a)
+deriving via (ViaHttpApiData (Last a))
+  instance FromHttpApiData a => FromMisoString (Last a)
+deriving via (ViaHttpApiData (Semi.Min a))
+  instance FromHttpApiData a => FromMisoString (Semi.Min a)
+deriving via (ViaHttpApiData (Semi.Max a))
+  instance FromHttpApiData a => FromMisoString (Semi.Max a)
+deriving via (ViaHttpApiData (Semi.First a))
+  instance FromHttpApiData a => FromMisoString (Semi.First a)
+deriving via (ViaHttpApiData (Semi.Last a))
+  instance FromHttpApiData a => FromMisoString (Semi.Last a)
+deriving via (ViaHttpApiData (Maybe a))
+  instance FromHttpApiData a => FromMisoString (Maybe a)
+deriving via (ViaHttpApiData (Either a b))
+  instance (FromHttpApiData a, FromHttpApiData b) => FromMisoString (Either a b)
+deriving via (ViaHttpApiData UUID.UUID) instance FromMisoString UUID.UUID
+deriving via (ViaHttpApiData (LenientData a))
+  instance FromHttpApiData a => FromMisoString (LenientData a)
+deriving via (ViaHttpApiData SetCookie) instance FromMisoString SetCookie
+deriving via (ViaHttpApiData (Tagged b a))
+  instance FromHttpApiData a => FromMisoString (Tagged b a)
+deriving via (ViaHttpApiData (Const a b))
+  instance FromHttpApiData a => FromMisoString (Const a b)
+deriving via (ViaHttpApiData (Identity a))
+  instance FromHttpApiData a => FromMisoString (Identity a)
+----------------------------------------------------------------------------
+
